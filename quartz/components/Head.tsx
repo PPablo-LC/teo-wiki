8f2de76e8ab1d666546a092c21ba6dd090b0644d
Pablo Redondo LC <ppabloredondolc@proton.me>
2026-06-07 00:36:12 -0300

    Quartz sync: Jun 7, 2026, 12:36 AM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -31,7 +31,7 @@ export default (() => {
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
     const usesCustomOgImage = false; //
-    )
+
     const ogImageDefaultPath = `https://${cfg.baseUrl}/static/og-image.png`
 
     const coreStylesheet = css[0]?.content
